eb5f8079887ae6c40775d90a1d75821692a93fd1
aftabbagwan <aftabbagwan9886@gmail.com>
2024-08-27 11:53:31 +0530

    tab bar design added

---
+++ lib/pages/drive.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RecordDrive extends StatefulWidget {
+  const RecordDrive({super.key});
+
+  @override
+  State<RecordDrive> createState() => _RecordDriveState();
+}
+
+class _RecordDriveState extends State<RecordDrive> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:recording/pages/drive.dart';
+import 'package:recording/pages/record.dart';
+import 'package:recording/utils/colors.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home"),
+          backgroundColor: AppColors.red,
+          bottom: TabBar(
+            indicatorColor: AppColors.white,
+            tabs: [
+              Tab(
+                  icon: Icon(
+                Icons.mic,
+                color: AppColors.white,
+              )),
+              Tab(
+                  icon: Icon(
+                Icons.play_arrow,
+                color: AppColors.white,
+              )),
+              Tab(
+                  icon: Icon(
+                Icons.settings,
+                color: AppColors.white,
+              )),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            const Record(),
+            const RecordDrive(),
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}
